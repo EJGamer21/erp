3df3d40e8554bd32f99ca24d7930eb7247abe596
Enger Jimenez <engersky12@gmail.com>
2019-06-21 15:48:13 -0400

    Edited meta tags

--- application/views/loader.php
+++ application/views/loader.php
@@ -8,7 +8,7 @@
         <meta name="application-name" content="ERP">
         <meta name="author" content="Enger Jimenez">
         <meta name="description" content="A web application to manage your enterprise">
-        <meta name="keywords" content="ERP, PHP, Javascript, JQuery, AJAX, Semantic UI, CSS">
+        <meta name="keywords" content="ERP, CRM, CodeIgniter, PHP, Javascript, JQuery, AJAX, Materialize, CSS">
         <link rel="icon" href="public/icons/icon.ico" type="image/x-icon">
         <link rel="author" href="https://github.com/EJGamer21/">
 
