ef5430d2a38b1ba23408b3d51a7d34a117fab320
EJGamer21 <engersky12@gmail.com>
2019-07-19 12:17:42 -0400

    Getter functions done

--- application/models/Directions_model.php
+++ application/models/Directions_model.php
@@ -8,26 +8,30 @@
         }
 
         function getDirections() {
-            $this->db->select('direcciones.*, provincias.*, ciudades.*, sectores.*');
-            $this->db->from($this->_tablename);
+            $fields = [
+                'direcciones.*',
+                'provincias.nombre as provinces', 
+                'ciudades.nombre as cities', 
+                'sectores.nombre as sectors'
+            ];
+
             $this->db->join('provincias', 'direcciones.provincia_id = provincias.id', 'left');
             $this->db->join('ciudades', 'direcciones.ciudad_id = ciudades.id', 'left');
             $this->db->join('sectores', 'direcciones.sector_id = sectores.id', 'left');
-            $sql = $this->db->get();
 
-            return $sql->result();
+            return $this->get(NULL, $fields);
         }
 
         function getDirection($id) {
             $fields = [
-                'direcciones.*', 
-                'provincias.*', 
-                'ciudades.*', 
-                'sectores.*'
+                'direcciones.*',
+                'provincias.nombre as provinces', 
+                'ciudades.nombre as cities', 
+                'sectores.nombre as sectors'
             ];
             // WATCH
             $conditions = [
-                $this->_tablename.'id' => $id
+                $this->_primary_key => $id
             ];
 
             $this->db->join('provincias', 'direcciones.provincia_id = provincias.id', 'left');
@@ -39,32 +43,49 @@
 
         function saveDirection($data, $id = NULL) {
             $direction = [
-                'provincia_id' => $this->_saveProvince($data['province']),
-                'ciudad_id' => $this->_saveCity($data['city']),
-                'sector_id' => $this->_saveSector($data['sector'])
+                'provincia_id' => $this->_saveProvince($data['province'])['id'],
+                'ciudad_id' => $this->_saveCity($data['city'])['id'],
+                'sector_id' => $this->_saveSector($data['sector'])['id']
             ];
-
             return $this->save($direction, $id);
+        }
+
+        function getProvinces() {
+            $sql = $this->db->get('provincias');
+
+            return $sql->result();
+        }
+
+        function getCities() {
+            $sql = $this->db->get('ciudades');
+
+            return $sql->result();
+        }
+
+        function getSectors() {
+            $sql = $this->db->get('sectores');
+
+            return $sql->result();
         }
 
         private function _saveProvince($provinceId) {
             $this->db->where('id', $provinceId);
             $sql = $this->db->get('provincias');
 
-            return $sql->row(0);
+            return $sql->row_array(0);
         }
 
         private function _saveCity($cityId) {
             $this->db->where('id', $cityId);
             $sql = $this->db->get('ciudades');
 
-            return $sql->row(0);
+            return $sql->row_array(0);
         }
 
         private function _saveSector($sectorId) {
             $this->db->where('id', $sectorId);
             $sql = $this->db->get('sectores');
 
-            return $sql->row(0);
+            return $sql->row_array(0);
         }
     }
