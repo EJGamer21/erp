e5d41ac7d53b3eaafead4373cadf7c06f707142d
EJGamer21 <engersky12@gmail.com>
2019-08-21 15:06:13 -0400

    Local libraries imported

--- application/views/loader.php
+++ application/views/loader.php
@@ -1,5 +1,4 @@
 <?php
-    $CI = &get_instance();
     $data = isset($data) ? $data : NULL;
 ?>
 <!DOCTYPE html>
@@ -12,17 +11,20 @@
         <meta name="application-name" content="ERP">
         <meta name="author" content="Enger Jimenez">
         <meta name="description" content="A web application to manage your enterprise">
-        <meta name="keywords" content="ERP, CRM, CodeIgniter, PHP, Javascript, jQuery, AJAX, Bootstrap, CSS, VueJS">
+        <meta name="keywords" content="ERP, CRM, CodeIgniter, PHP, Javascript, AJAX, Axios, Bootstrap, CSS, VueJS">
         <link rel="icon" href="/public/icons/icon.ico" type="image/x-icon">
         <link rel="author" href="https://github.com/EJGamer21/">
 
     <!-- Libraries -->
+        <!-- VueJS -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.10/vue.js"></script>
         
-        <script src="/public/libs/js/axios.min.js"></script>
-        <script src="/public/libs/js/toastr/toastr.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/vue-tables-2@1.4.70/dist/vue-tables-2.min.js"></script>
+        <!-- Axios -->
+        <script src="/public/libs/js/axios/axios.min.js"></script>
 
+        <!-- Vue Tables 2 -->
+        <script src="https://cdn.jsdelivr.net/npm/vue-tables-2@1.4.70/dist/vue-tables-2.min.js"></script>
+        
         <!-- Bootstrap -->
         <link type="text/css" rel="stylesheet" href="/public/libs/css/bootstrap/bootstrap.min.css"/>
         <link type="text/css" rel="stylesheet" href="/public/libs/css/bootstrap/bootstrap-vue.min.css"/>
@@ -32,10 +34,16 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/solid.css" integrity="sha384-QokYePQSOwpBDuhlHOsX0ymF6R/vLk/UQVz3WHa6wygxI5oGTmDTv8wahFOSspdm" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/brands.css" integrity="sha384-n9+6/aSqa9lBidZMRCQHTHKJscPq6NW4pCQBiMmHdUCvPN8ZOg2zJJTkC7WIezWv" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/fontawesome.css" integrity="sha384-vd1e11sR28tEK9YANUtpIOdjGW14pS87bUBuOIoBILVWLFnS+MCX9T6MMf0VdPGq" crossorigin="anonymous">
-
+        
         <!-- Toastr -->
-        <link rel="stylesheet" href="/public/libs/css/toastr/toastr.min.css"></link>
-        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+        <script src="/public/libs/js/toastr/toastr.min.js"></script>
+        <link rel="stylesheet" href="/public/libs/css/toastr/toastr.min.css"/>
+
+        <!-- Selectpage -->
+        <script src="/public/libs/js/selectpage/v-selectpage.js"></script>
+
+        <!-- Sweetalert -->
+        <script src="/public/libs/js/sweetalert/sweetalert.min.js"></script>        
     <title><?= $title ?> | Fractal</title>
 
     <style>
