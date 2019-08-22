08bb354ea661fae5b62db4a9a029716f047940e5
EJGamer21 <engersky12@gmail.com>
2019-08-22 16:03:39 -0400

    User view started

--- application/views/users/view.php
+++ application/views/users/view.php
@@ -1,3 +1,42 @@
-<div>
-    <h1><?= $user->firstname.' '.$user->lastname ?></h1>
+<div id="vueapp" v-cloak>
+    <div class="card my-2">
+        <div class="row no-gutters">
+            <div class="col-md-4">
+                <img :src="user.image" alt="broken image :(">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h3 class="card-title">
+                        <span>{{ user.firstname + ' ' + user.lastname}}
+                        <span class="badge badge-light">{{ user.rol }}</span
+                    </h3>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+<script>
+    const userId = <?= $userId ?>;
+    const app = new Vue({
+        el: '#vueapp',
+        data() {
+            return {
+                user: {},
+            }
+        },
+        mounted() {
+            axios.get('/users/get/' + userId, {
+                responseType: 'json'
+            })
+            .then((response) => {
+                const user = response.data.response;
+                this.user = user;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        },        
+    });
+</script>
